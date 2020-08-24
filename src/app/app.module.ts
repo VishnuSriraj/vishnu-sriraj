4e97965376564c607360528bf850bae96cd4e418
Vishnu Sriraj Tirupur Varadharaj <vishnusriraj@Vishnus-MBP.fios-router.home>
2020-08-23 23:32:39 -0400

    Third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { IntroComponent } from './intro/intro.component';
+import { KnowmeComponent } from './knowme/knowme.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    IntroComponent,
+    KnowmeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/knowme/knowme.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-knowme',
+  templateUrl: './knowme.component.html',
+  styleUrls: ['./knowme.component.scss']
+})
+export class KnowmeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
