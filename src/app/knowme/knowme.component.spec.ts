15efd320c91426aca4749e0f6491022ad97a8d9e
VishnuSriraj <vishnu.sriraj98@gmail.com>
2020-12-18 19:15:24 -0500

    Launch ready

--- src/app/knowme/knowme.component.spec.ts
+++ src/app/knowme/knowme.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { KnowmeComponent } from './knowme.component';
 
@@ -6,7 +6,7 @@ describe('KnowmeComponent', () => {
   let component: KnowmeComponent;
   let fixture: ComponentFixture<KnowmeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KnowmeComponent ]
     })
